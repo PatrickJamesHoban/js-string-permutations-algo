37c6339828d02e7676008305a23ef677e1b8ef08
PatrickJamesHoban <Patrick.J.Hoban@gmail.com>
2018-02-22 15:23:37 -0600

    included other solutions for review

--- permutations_recursion.js
+++ permutations_recursion.js
@@ -114,6 +114,21 @@ permutations('aabc'); // ['cbaa', 'caab', 'caba', 'abac', 'abca', 'aacb', 'aabc'
 // permutations('ababc');  // 
 
 
+// ********************************************** OTHER SOLUTIONS ****************************************************
+function permutations(string) {
+  var arr = string.split(''), tmp = arr.slice(), heads = [], out = [];
+  if(string.length == 1) return [string];
+  arr.forEach(function(v, i, arr) {
+    if(heads.indexOf(v) == -1) {
+      heads.push(v);
+      tmp.splice(tmp.indexOf(v), 1);
+      permutations(tmp.join('')).forEach(function(w) {out.push(v + w);});
+      tmp.push(v);
+    }
+  });
+  return out;
+}
+
 
 // *************************************** ORIGINAL CODE THOUGHT PROCESS *********************************************
 
